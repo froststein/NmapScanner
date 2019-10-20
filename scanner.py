fac955cfecd3f80e14a3397801c312cdd241acd6
Roy <15943697+froststein@users.noreply.github.com>
2019-10-20 14:06:45 +0800

    Add files via upload

--- scanner.py
+++ scanner.py
@@ -1,35 +1,69 @@
+#!/usr/bin/env python
+
+
+__description__ = 'nmap scanner'
+__author__ = 'Froststein'
+__version__ = '0.2'
+
 import nmap
 import csv
+import optparse
+import time
+import os
 
-# Read stored values to scan with nmap
-def readCSV():
-    # declare list to store IP Address / Web Addresses
+# Read stored values for scanNmapCSV()
+def readInput(inputfile):
     ip_list=[]
-    #further improvements could be made for line 8
-    with open('test.csv', mode='r') as csv_file:
+    with open(inputfile, mode='r') as csv_file:
         csv_reader = csv.reader(csv_file)
         for row in csv_reader:
             if row:
                 ip_list+=[f'{"".join(row)}']
     return ip_list
-
-def scan():
     
-    scanresult_list=[]
+def saveCSV(result,outputfile):
+    with open('scan/'+outputfile,'a',newline='') as csvfile:
+        reader=csv.writer(csvfile)
+        reader.writerow(result)
+    csvfile.close()
     
-    ip_list = readCSV()
-    print("List to scan: ", ip_list)
+def scanNmapXML(inputfile,outputfile):
+    command="nmap -iL "+ inputfile +" -oX logs/"+outputfile+".xml"
+    os.system(command)
+    
+def scanNmapCSV(inputfile,outputfile):
+    ip_list = readInput(inputfile)
     print("Scanner Starting.......")
     ns = nmap.PortScanner()
-    ns.scan(ip_list[0],'1-1024','-v -sS')
-    print(ns.scaninfo())
-    print("IP Status: ",ns[ip_list[0]].state())
-    print(ns[ip_list[0]].all_protocols())
-    print("Open Ports: ", ns[ip_list[0]]['tcp'].keys())
-    scanresult_list+=[ns[ip_list[0]]['tcp'].keys()]
-    print(scanresult_list)
-    
-    
+    outputfile=outputfile+'.csv'
+    for ip in ip_list:
+        ns.scan(ip,'1-1024','-v -sS ')
+        print(ns.scaninfo())
+        print("IP Status: ",ns[ip].state())
+        print("Open Ports: ", ns[ip]['tcp'].keys())
+        lports=ns[ip]['tcp'].keys()
+        RESULT=[]
+        RESULT.append(ip)
+        port_list=[]
+        for port in lports:
+            print('port : %s\tstate : %s' % (port, ns[ip]['tcp'][port]['state']))
+            port_list.append('%s|%s'%(port,ns[ip]['tcp'][port]['state']))
+        RESULT.append(port_list)
+        saveCSV(RESULT,outputfile)
+        print(RESULT)
+        
+def initializeScan(inputfile,outputfile):
+    try:
+        f=open(inputfile)
+        scanNmapCSV(inputfile,outputfile)
+        scanNmapXML(inputfile,outputfile)
+        f.close()
+    except FileNotFoundError:
+        print('File specified not found')
+        inputfile = input('Enter the file name again: ')
+        initializeScan(inputfile,outputfile)
+
+        
 def banner():
 	banner = '''                                                                 ____    ___  
   _  _   __  __    ___      ___             ___                                                   
@@ -40,11 +74,20 @@ _|"""""|_|"""""|_|"""""|_| """ | {======|_|"""""|_|"""""|_|"""""|_|"""""|_|"""""
 "`-0-0-'"`-0-0-'"`-0-0-'"`-0-0-'./o--000'"`-0-0-'"`-0-0-'"`-0-0-'"`-0-0-'"`-0-0-'"`-0-0-'"`-0-0-'                                                 
 	'''
 	print (banner)
-    
+
 
 def main():
     banner()
-    scan()
+    oParser = optparse.OptionParser(usage='usage: %prog \n' + __description__, version='%prog ' + __version__)
+    oParser.add_option('-i', '--input',dest='inputfile', type='str', help='Input file')
+    
+    (options, args) = oParser.parse_args()
+    if ((options.inputfile != None)):
+        #declare file name for scan/.csv and logs/.xml
+        OUTPUTFILE=time.strftime("%Y%m%d-%H%M%S")
+        initializeScan(options.inputfile,OUTPUTFILE)
+    else:
+        oParser.print_help()
     
     
 if __name__ == "__main__":
